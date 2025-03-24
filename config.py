ee8d7b1faca9aefb9d68b9afd5dc26891c599e38
master <master@admin.com>
2025-03-24 15:19:17 +0800

    feat(trading): 添加市价止盈功能并支持交割合约

--- config.py
+++ config.py
@@ -6,7 +6,12 @@ BINANCE_CONFIG = {
     'base_url': 'https://fapi.binance.com' if env == 'prod' else 'https://testnet.binancefuture.com',
     'ip_white_list': ['52.89.214.238', '34.212.75.30', '54.218.53.128', '52.32.178.7', '127.0.0.1']
 }
-
+BINANCE_CM_CONFIG={
+    'key': '8a75263f1d18ef611dd4e920215cb71d47474f5e1168fab6023b3ba929fc7b5e',
+    'secret': 'de82f4cce06f0aafc520ece0786b6f389edef4d602c19aef33ab08fd8c7f4a1b',
+    'base_url': 'https://dapi.binance.com' if env == 'prod' else 'https://testnet.binancefuture.com',
+    'ip_white_list': ['52.89.214.238', '34.212.75.30', '54.218.53.128', '52.32.178.7', '127.0.0.1']
+}
 # 微信通知配置
 WX_CONFIG = {
     'token': '8nEhpKFjhU9uKaDDnfDseWy1P'
